9290ab9be74805a016245141ab9aad02ad2d29b8
Jovan Medford <jovanmedford@gmail.com>
2021-10-25 09:49:45 -0400

    Chore: Update sitemetadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Jovan Medford`,
+    description: `Hi! I'm a frontend dev that likes to build and design beautiful web apps.`,
+    author: `Jovan Medford`,
     siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
   },
   plugins: [
