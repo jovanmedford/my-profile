af15a1accf8947d2e31ffdbe9e869eceb25a5eb4
jovanmedford <jovanmedford@gmail.com>
2021-11-06 20:13:45 -0400

    Chore: Tweak headings and spacing

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,33 +9,38 @@ export default function () {
     <Layout>
       <Seo title="Jovan Medford" />
       <div tw="mx-4">
-        <h1 tw="text-2xl font-bold text-center">
+        <h1 tw="text-3xl font-bold text-center">
           I design and build beautiful web apps.
         </h1>
       </div>
-      <section tw="mt-28">
-        <h2 tw="text-center text-lg md:text-xl font-bold mb-6">Portfolio</h2>
+      <section tw="mt-44">
+        <h2 id="portfolio" tw="text-center text-lg md:text-2xl font-bold mb-8">
+          Portfolio
+        </h2>
         <div tw="mx-4 md:flex md:mx-12 lg:mx-auto justify-between max-w-4xl ">
           <Card
             tw="lg:w-6/12 mr-8"
-            ImageComponent={susuImage}
-            title="SUSU Magazine"
-          />
-          <Card
-            tw="lg:w-6/12"
+            to="/caribbean-scholar"
             ImageComponent={caribbeanScholarImage}
             title="Caribbean Scholar"
+          />
+          <Card
+            to="/susumagazine"
+            tw="lg:w-6/12"
+            ImageComponent={susuImage}
+            title="SUSU Magazine"
           />
         </div>
       </section>
       <section
+        id="about-me"
         tw="mt-20 text-white bg-primary-700 
             pt-16 pl-10 pb-10 pr-5
             mx-4 md:mx-12
             lg:mx-auto max-w-4xl
             md:pr-8 rounded-lg"
       >
-        <h2 tw="text-lg font-bold mb-5">About me</h2>
+        <h2 tw="text-lg md:text-2xl font-bold mb-5">About me</h2>
         <TextContainer>
           <p>I’m a Front-End Developer from sunny Barbados!</p>
           <p>
@@ -57,6 +62,6 @@ export default function () {
 
 const TextContainer = styled("div")`
   p {
-    ${tw`mb-4 leading-relaxed`}
+    ${tw` md:text-md mb-4 leading-relaxed`}
   }
 `
